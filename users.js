2979218b8bb1de693b63ae53c181afcda405bd27
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-09 07:17:45 +0100

    added passport, db, router

--- users.js
+++ users.js
@@ -1,16 +1,29 @@
 'use strict';
 
-//var express = require('express')
-  //, router = express.Router();
-  //, log = require('magic-log')
-  //, passport = require('passport')
-//  , local = require('passport-local').Strategy
-//;
+var express = require('express')
+  , router = express.Router();
+  , log = require('magic-log')
+  , db = require('magic-db')
+  , passport = require('passport')
+  , local = require('passport-local').Strategy
+;
 
 //middleware function
 module.exports = function (req, res, next) {
   log('users called');
-  
+  passport.use(new local(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
 
   next();
 }
