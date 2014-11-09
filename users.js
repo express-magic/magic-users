d3d692c0034d5cda2c591141cfc5ed16ab8904b4
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-09 07:11:34 +0100

    first commit

---
+++ users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//var express = require('express')
+  //, router = express.Router();
+  //, log = require('magic-log')
+  //, passport = require('passport')
+//  , local = require('passport-local').Strategy
+//;
+
+//middleware function
+module.exports = function (req, res, next) {
+  log('users called');
+  
+
+  next();
+}
