12eea3de649d523f7e678764f9b192ac53083322
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-12 06:10:16 +0100

    users model expanded, removed auth

--- users.js
+++ users.js
@@ -1,31 +1,69 @@
 'use strict';
 
-var express = require('express')
-  , router = express.Router();
-  , log = require('magic-log')
-  , db = require('magic-db')
-  , passport = require('passport')
-  , local = require('passport-local').Strategy
-;
-
-
+var log = require('magic-log');
 
 //middleware function
-module.exports = function (req, res, next) {
-  log('users called');
-  
-  passport.use(new local(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
+exports.init = function (schema, next) {
+  var User, Meta, Roles;
+
+  User = schema.define( 'User', {
+    name:  { type: String, length: 23, index: true }
+  , email: { type: String, length: 100, index: true }
+  , pass:  { type: String, length: 100 }
+  });
+
+  User.validatesPresenceOf('name', 'email', 'pass');
+  User.validatesUniquenessOf('name', {message: 'name already exists'});
+  User.validatesLengthOf('pass', {
+    min: 10, 
+    message: {
+      min: 'password too short (min of ' + 10 + ' chars)'}
+    }
+  );
+  User.validatesUniquenessOf('email', { message: 'email already in use' });
+
+  User.prototype.validatePassword = function(name, pass, next) {
+    User.findOne({name: name, pass: pass}, function(err, user) {
+      next(err, user);
     });
   }
+
+  User.prototype.add = function (user, cb) {
+    var user = new User({
+          name: user.name
+        , email: user.email
+        , pass: user.pass
+      })
+      , roles = new Roles(user.roles)
+      , meta = new Meta(user.meta)
+    ;
+
+    async.series([
+        user.save
+      , roles.save
+      , meta.save
+    ], function (err, results) {
+      console.log('user add done');
+    });
+  }
+
+  Meta = schema.define('UserMeta', {
+    bio     : { type: Schema.Text }
+  , joinDate: { type: Date, default: Date.now }
+  });
+
+  Roles = schema.define('UserRoles', {
+    host:   { type: String, index: true }
+  , role:   { type: String, default: 'subscriber', index: true }
+  });
+  
+
+  Roles.validatesInclusionOf('role', {in: ['subscriber', 'editor', 'administrator']});
+
+  User.hasMany(Roles, { as: 'roles', foreignKey: 'userId' } );
+
+  User.prototype.meta = Meta;
+  User.prototype.Roles = Roles;
+
   next();
 }
